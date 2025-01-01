785ad4a1814872b7e1bba864e276e99ad749fa8a
Lisa Boettcher <153122203+member888lb@users.noreply.github.com>
2024-12-31 17:16:48 -0800

    Add files via upload

---
+++ Python Coding Practices and Packaging Concepts/geometry.py
@@ -0,0 +1,20 @@
+# length and width of rectangular room
+length = 10
+width = 12
+
+#computing area of rectangular room
+room_area = length * width
+
+print('The area of rectangular room in meters is :', room_area)
+
+# Define the radius
+radius = 5
+
+# Define the value of Pi
+pi = 3.14159
+
+# Calculate the area
+area = pi * (radius **2)
+
+# Print the result
+print(f"The area of the circle with radius{radius} is {area}")
